bd60a9bc90d96b399fef463675f98f9165c42ac9
valentina-b <valentina.bedi@gmail.com>
2020-12-26 21:27:15 +0000

    add about page

--- ecosio/settings.py
+++ ecosio/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'homepage',
+    'about'
 ]
 
 SITE_ID = 1
--- ecosio/urls.py
+++ ecosio/urls.py
@@ -21,5 +21,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include ('allauth.urls')),
-    path('', include ('homepage.urls'))
+    path('', include ('homepage.urls')),
+    path('about/', include ('about.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
