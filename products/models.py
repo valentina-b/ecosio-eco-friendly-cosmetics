d9bf2aa69eb4f3e28d4145bed6a4cf08f267e7da
valentina-b <valentina.bedi@gmail.com>
2021-01-01 18:24:01 +0000

    add Products model, format products.json, create media folder with product images

--- products/models.py
+++ products/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 
+
 class Category(models.Model):
 
     class Meta:
@@ -31,3 +32,26 @@ class Brand(models.Model):
 
     def get_short_name(self):
         return self.short_name
+
+
+class Product(models.Model):
+
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    brand = models.ForeignKey('Brand', null=True, blank=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=55)
+    description = models.TextField(max_length=375)
+    ingredients = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    is_eco = models.BooleanField(default=True, null=True, blank=True)
+    is_crueltyfree = models.BooleanField(default=True, null=True, blank=True)
+    is_vegan = models.BooleanField(default=True, null=True, blank=True)
+    is_organic = models.BooleanField(default=True, null=True, blank=True)
+    is_natural = models.BooleanField(default=True, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    class Meta:
+        ordering = ["pk"]
+
+    def __str__(self):
+        return self.name
