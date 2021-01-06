265acb886fd961108a8f4ddc77186cd3297f1587
valentina-b <valentina.bedi@gmail.com>
2021-01-06 16:15:12 +0000

    create 3 random products for the product page recommendation section

--- products/views.py
+++ products/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 from .models import Product, Category
+import random
 
 # Create your views here.
 
@@ -54,8 +55,25 @@ def product_page(request, product_id):
 
     product = get_object_or_404(Product, pk=product_id)
 
+    # get the product's category
+    product_category = product.category
+    # select all products in the system
+    all_products = Product.objects.all()
+    # filter out from all products those that match the product's category
+    category_filtered_products = all_products.filter(category=product_category)
+    # get 3 random projects from that category
+    three_random_products = random.sample(list(category_filtered_products), 3)
+    # create more intuitive variables to be used in jinja templates
+    random_product_1 = three_random_products[0]
+    random_product_2 = three_random_products[1]
+    random_product_3 = three_random_products[2]
+
     context = {
         'product': product,
+        'product_category': product_category,
+        'random_product_1': random_product_1,
+        'random_product_2': random_product_2,
+        'random_product_3': random_product_3,
     }
 
     return render(request, 'products/product_page.html', context)
