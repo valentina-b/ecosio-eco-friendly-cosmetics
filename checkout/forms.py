0467c630e43dcd1210afe7cfa69f64ca79937cfb
valentina-b <valentina.bedi@gmail.com>
2021-01-12 18:06:42 +0000

    style dsk and tab checkout form

--- checkout/forms.py
+++ checkout/forms.py
@@ -28,7 +28,7 @@ class OrderForm(forms.ModelForm):
         self.fields['full_name'].widget.attrs['autofocus'] = True
         for field in self.fields:
             if self.fields[field].required:
-                placeholder = f'{placeholders[field]} *'
+                placeholder = f'{placeholders[field]}'
             else:
                 placeholder = placeholders[field]
             self.fields[field].widget.attrs['placeholder'] = placeholder
