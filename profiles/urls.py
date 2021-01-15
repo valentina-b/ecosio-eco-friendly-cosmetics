0cf19899fc31af20f24c755a280d4e66c579a6e5
valentina-b <valentina.bedi@gmail.com>
2021-01-15 14:37:08 +0000

    rearange profile views/urls and adjust breadcrumbs

--- profiles/urls.py
+++ profiles/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.profile, name='profile')
+    path('', views.profile, name='profile'),
+    path('edit_profile/', views.edit_profile, name='edit_profile'),
+    path('order_history/', views.order_history, name='order_history'),
+    path('loyalty_status/', views.loyalty_status, name='loyalty_status'),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -8,3 +8,30 @@ def profile(request):
     context = {}
 
     return render(request, template, context)
+
+
+def order_history(request):
+    """ Display the user's order history. """
+
+    template = 'profiles/order_history.html'
+    context = {}
+
+    return render(request, template, context)
+
+
+def edit_profile(request):
+    """ Display the user's edit profile details form. """
+
+    template = 'profiles/edit_profile_details.html'
+    context = {}
+
+    return render(request, template, context)
+
+
+def loyalty_status(request):
+    """ Display the user's edit profile details form. """
+
+    template = 'profiles/loyalty_status.html'
+    context = {}
+
+    return render(request, template, context)
