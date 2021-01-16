38d56cd6a8668e5daf4b6b97c158cc8b21a6ba38
valentina-b <valentina.bedi@gmail.com>
2021-01-16 18:54:56 +0000

    import login_required, adjust edit_profile logic

--- profiles/views.py
+++ profiles/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
-
+from django.contrib.auth.decorators import login_required
 from .models import UserProfile
 from .forms import UserProfileForm
 
+from checkout.models import Order
 
+@login_required
 def profile(request):
     """ Display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
@@ -15,6 +17,7 @@ def profile(request):
     return render(request, template, context)
 
 
+@login_required
 def order_history(request):
     """ Display the user's order history. """
     profile = get_object_or_404(UserProfile, user=request.user)
@@ -28,6 +31,7 @@ def order_history(request):
     return render(request, template, context)
 
 
+@login_required
 def edit_profile(request):
     """ Display the user's edit profile details form. """
     profile = get_object_or_404(UserProfile, user=request.user)
@@ -37,20 +41,23 @@ def edit_profile(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Profile updated successfully')
-
-    form = UserProfileForm(instance=profile)
+        else:
+            messages.error(request, 'Update failed. Please ensure the form is valid.')
+    else:
+        form = UserProfileForm(instance=profile)
 
     template = 'profiles/edit_profile_details.html'
     context = {
         'form': form,
-        'on_profile_page': True
     }
 
     return render(request, template, context)
 
 
+@login_required
 def loyalty_status(request):
     """ Display the user's edit profile details form. """
+    profile = get_object_or_404(UserProfile, user=request.user)
 
     template = 'profiles/loyalty_status.html'
     context = {}
