2a9c14d8fc18c282b05b095160b9f9e5b74f6db8
valentina-b <valentina.bedi@gmail.com>
2021-01-17 14:42:29 +0000

    add admin edit_product functionality

--- products/urls.py
+++ products/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.all_products, name='products'),
     path('<int:product_id>/', views.product_page, name='product_page'),
     path('add_product/', views.add_product, name='add_product'),
+    path('edit_product/<int:product_id>/', views.edit_product, name='edit_product'),
 ]
