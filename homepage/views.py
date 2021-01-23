37b8be9645b52b98af973da96971ed05ed784765
valentina-b <valentina.bedi@gmail.com>
2021-01-23 16:51:14 +0000

    improve hp featured products gallery

--- homepage/views.py
+++ homepage/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from products.models import Product
 
 # Create your views here.
 
+
 def index(request):
     """ A view to return the index page """
+    featured_product_1 = get_object_or_404(Product, pk=2)
+    featured_product_2 = get_object_or_404(Product, pk=43)
+    featured_product_3 = get_object_or_404(Product, pk=56)
 
-    return render(request, 'homepage/index.html')
+    template = 'homepage/index.html'
+    context = {
+        'featured_product_1': featured_product_1,
+        'featured_product_2': featured_product_2,
+        'featured_product_3': featured_product_3,
+    }
+
+    return render(request, template, context)
