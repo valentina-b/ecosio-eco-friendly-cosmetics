619c30beb33dd136fbe3514f335aada15cfb1017
valentina-b <valentina.bedi@gmail.com>
2021-01-24 21:52:16 +0000

    prepare for heroku deployment

--- ecosio/settings.py
+++ ecosio/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'f*9v))7o3y57j9#idfz)tl*#dtj09jlhy0&l@f389fai=##&qx'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ecosio.herokuapp.com', localhost]
 
 
 # Application definition
@@ -123,12 +123,17 @@ WSGI_APPLICATION = 'ecosio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
