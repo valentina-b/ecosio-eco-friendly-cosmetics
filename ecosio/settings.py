c68ec18a9ff5648ba615b639c928a6d4cd32d547
valentina-b <valentina.bedi@gmail.com>
2021-01-25 13:58:43 +0000

    correct static img sources; correct order email confirmation text

--- ecosio/settings.py
+++ ecosio/settings.py
@@ -96,6 +96,7 @@ TEMPLATES = [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth', # required by allauth
+                'django.template.context_processors.static',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
                 'cart.contexts.cart_contents',
