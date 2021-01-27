49e61cd0b8b46657f53e6cb91442a95e266c40d6
valentina-b <valentina.bedi@gmail.com>
2021-01-27 21:15:31 +0000

    finish js scripts updating with the help of JSHint

--- cart/static/cart/js/modify_quantity.js
+++ cart/static/cart/js/modify_quantity.js
@@ -2,7 +2,7 @@
 $('.update-link').click(function(e) {
     let form = $(this).prev('.update-form');
     form.submit();
-})
+});
 
 // Remove item and reload on click
 $('.remove-item').click(function(e) {
@@ -15,4 +15,4 @@ $('.remove-item').click(function(e) {
     .done(function() {
         location.reload();
     });
-})
+});
