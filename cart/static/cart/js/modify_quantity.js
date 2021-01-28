c0cba65a6675d3e74248e19359b1f3612f79c132
valentina-b <valentina.bedi@gmail.com>
2021-01-28 10:02:56 +0000

    put remove-item script back to template (to pass csrf token properly)

--- cart/static/cart/js/modify_quantity.js
+++ cart/static/cart/js/modify_quantity.js
@@ -3,16 +3,3 @@ $('.update-link').click(function(e) {
     let form = $(this).prev('.update-form');
     form.submit();
 });
-
-// Remove item and reload on click
-$('.remove-item').click(function(e) {
-    let csrfToken = "{{ csrf_token }}";
-    let itemId = $(this).attr('id').split('remove_')[1];
-    let url = `/cart/remove/${itemId}/`;
-    let data = {'csrfmiddlewaretoken': csrfToken};
-
-    $.post(url, data)
-    .done(function() {
-        location.reload();
-    });
-});
