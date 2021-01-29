88a64ff64055cb51ebe59173a270c1f5482cb985
valentina-b <valentina.bedi@gmail.com>
2021-01-29 14:09:55 +0000

    adjust max 99 same products add_to_cart code so it works on product_page

--- cart/views.py
+++ cart/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect, reverse, HttpResponse, get_object
 from django.contrib import messages
 from products.models import Product
 
+from cart.contexts import cart_contents
+
 # Create your views here.
 
 
@@ -20,10 +22,43 @@ def add_to_cart(request, item_id):
     cart = request.session.get('cart', {})
 
     if item_id in list(cart.keys()):
-        # can't have more than 99 same products in the cart
+        current_cart = cart_contents(request)
+        current_cart_items = current_cart['cart_items']
+        current_cart_item_quantity = 0
+
+        # can't add more if there already are 99 products in the cart
         if cart[item_id] >= 99:
             messages.info(request, f'You can order up to 99 same products per order')
-            cart[item_id] = 99
+
+        # PDPs need a different solution
+        # their input is not updating quantity like cart- it's adding on top
+        # can't add on top if sum of what's in the cart and what you're adding is higher than 99
+        elif '/shop/' in redirect_url:
+            request_dict = request.POST
+            request_dict_quantity = request_dict['quantity']
+            try:
+                request_dict_product_id = redirect_url.split('/')[2]
+                request_dict_quantity = int(request_dict_quantity)
+                request_dict_product_id = int(request_dict_product_id)
+            except ValueError:
+                # because redirect url at shop (/shop/) doesn't provide product ID info
+                # case for adding a product directly from the feed
+                request_dict_product_id = int(item_id)
+                request_dict_quantity = int(request_dict_quantity)
+            for current_cart_item in current_cart_items:
+                for cart_item_id in current_cart_item['item_id']:
+                    cart_item_id = int(cart_item_id)
+                    if cart_item_id == request_dict_product_id:
+                        current_cart_item_quantity = int(current_cart_item['quantity'])
+                        quantity_sum = current_cart_item_quantity + request_dict_quantity
+                        if quantity_sum > 99:
+                            cart[item_id] = 99
+                            messages.info(request, f'You can order up to 99 items of the same product per order. We have updated {product.name.title()} quantity to {cart[item_id]}.')
+                        else:
+                            request_dict_product_id = item_id
+                            cart[item_id] += quantity
+                            messages.success(request, f'Updated {product.name.title()} quantity to {cart[item_id]}')
+                        return redirect(redirect_url)
         else:
             cart[item_id] += quantity
             messages.success(request, f'Updated {product.name.title()} quantity to {cart[item_id]}')
